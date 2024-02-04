38593d760705c131405cd36d818b72efa9241543
laurabacon <laura_kessinger@yahoo.com>
2024-02-04 10:53:31 -0700

    fixed save to local storage and get from local storage

--- Develop/script.js
+++ Develop/script.js
@@ -5,19 +5,20 @@ $(function () {
   let rightNow = currentDate.toLocaleDateString(undefined, options);
   $("#currentDay").html(rightNow);
 
-});
+
 
 // Saves to local storage
 $(".saveBtn").click(function(event) {
     event.preventDefault();
-    var value = $(this).siblings(".time-block").val();
-    var time = $(this).parent().attr("id").split("-")[1];
+    let value = $(this).siblings(".description").val();
+    let time = $(this).parent().attr("id");
     localStorage.setItem(time, value);
 });
 
 // gets from local storage
 $(".time-block").each(function() {
-    var time = $(this).parent().attr("id").split("-")[1];
-    $(this).val(localStorage.getItem(time));
+  let time = $(this).attr("id");
+  $(this).find(".description").val(localStorage.getItem(time));
 });
 
+});
