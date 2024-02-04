29908a9b61f88cb6c69519daa017b033a0fd2906
laurabacon <laura_kessinger@yahoo.com>
2024-02-04 10:41:05 -0700

    fixed date and time

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,23 @@
 $(function () {
   // Display current date
-  let rightNow = moment().format("dddd, MMMM Do YYYY");
+  let currentDate = new Date();
+  let options = { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric' };
+  let rightNow = currentDate.toLocaleDateString(undefined, options);
   $("#currentDay").html(rightNow);
-  let currentHour = moment().format("HH");
-  $(".time-block").each(function() {
-    var currentTimeDiv = $(this).attr("id").split("-")[1];
-    $(this).toggleClass("present", currentHour == currentTimeDiv);
-    $(this).children(".description").toggleClass("present", currentHour == currentTimeDiv);
-  });
+
 });
+
+// Saves to local storage
+$(".saveBtn").click(function(event) {
+    event.preventDefault();
+    var value = $(this).siblings(".time-block").val();
+    var time = $(this).parent().attr("id").split("-")[1];
+    localStorage.setItem(time, value);
+});
+
+// gets from local storage
+$(".time-block").each(function() {
+    var time = $(this).parent().attr("id").split("-")[1];
+    $(this).val(localStorage.getItem(time));
+});
+
